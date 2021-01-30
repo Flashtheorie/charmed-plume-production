3bbd139a339aa4daeb69450c555b0f427afff4a1
Antoine <antoinepascualmail@gmail.com>
2021-01-30 12:57:40 +0100

    Ajout navbar - Page d'accueil

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Accueil</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body >
+<style>
+<style>
+
+</style>
+</style>
+
+<?php include_once 'navbar.php'; ?>
+
+<div class="background_img">
+	<img src="assets/img/background.png" alt="background_img" class="img-fluid">
+	
+</div>
+
+
+</div>
+<div class="container">
+	<br>
+	<div class="jumbotron " align="center">
+		<h1>Charmed Plume Production</h1>
+		<hr>
+		<p>Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Magnam, quia, hic tempore molestias sit, perspiciatis vero dicta, atque illo explicabo iure? Ratione molestias magni assumenda fugit eveniet quasi adipisci ullam .Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<button type="button" class="btn btn-primary">S'inscrire</button>
+<button type="button" class="btn btn-primary">Voir les évènements</button>
+	</div>
+
+
+</div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">
+    <img src="assets/img/feather.png" alt="logo" style="width: 30px">
+
+   Charmed Plume Production</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Qui sommes nous ?</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="#">Evenements</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
